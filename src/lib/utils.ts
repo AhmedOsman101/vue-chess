0eb3fd104576008c0545b1392a35108a16930f0c
AhmadOthman <trollingosman101@outlook.com>
2025-01-22 09:33:30 +0200

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,17 +5,10 @@ export function* range(
   start = 0,
   end = Infinity,
   step = 1
-): Generator<number, number, unknown> {
-  let iterationCount = 0;
-  // for (let i = start; i != end; i += step) {
-  //   iterationCount++;
-  //   yield i;
-  // }
+): Generator<number, void, unknown> {
   let i = start;
   while (i != end) {
-    iterationCount++;
     yield i;
     i += step;
   }
-  return iterationCount;
 }
