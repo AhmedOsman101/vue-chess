8fb3372a607d31abaff95dedfc0efbce27158d44
AhmedOsman101 <trollingosman101@outlook.com>
2025-01-24 01:51:49 +0200

    refactor(Board): Improve Board component structure
    - Improved code formatting
    - Updated file import path
    - Minor UI adjustments

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,23 +1,37 @@
 <script setup lang="ts">
 import { range } from "@/lib/utils";
-import { getFile } from "@/lib/chess";
+import { getFile } from "@/lib/chess/notation";
 import Piece from "./Piece.vue";
 </script>
 
 <template>
   <section class="board-container">
     <div class="ranks">
-      <p v-for="rank of range(8, 0, -1)" v-text="rank" :key="rank" class="mx-auto" />
+      <p
+        v-for="rank of range(8, 0, -1)"
+        v-text="rank"
+        :key="rank"
+        class="mx-auto" />
     </div>
-    <div class="border" id="board" ref="board">
+    <div
+      class="border"
+      id="board"
+      ref="board">
       <div v-for="col of range(0, 8)">
         <div v-for="row of range(0, 8)">
-          <Piece :key="`${row}-${col}`" :row="row" :col="col" />
+          <Piece
+            :key="`${row}-${col}`"
+            :row="row"
+            :col="col" />
         </div>
       </div>
     </div>
     <div class="files">
-      <p v-for="file of range(0, 8)" :key="file" v-text="getFile(file)" class="mx-auto" />
+      <p
+        v-for="file of range(0, 8)"
+        :key="file"
+        v-text="getFile(file)"
+        class="mx-auto" />
     </div>
   </section>
 </template>
