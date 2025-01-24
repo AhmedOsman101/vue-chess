8a81f263cd4b36093b6dbd28eb0e33bd8132720c
AhmedOsman101 <trollingosman101@outlook.com>
2025-01-24 02:45:42 +0200

    feat(ui): add move history component
    - Created MoveHistory.vue component
    - Displays game move history
    - Uses game store for data

---
+++ src/components/MoveHistory.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useGameStore } from "@/stores/game";
+
+const gs = useGameStore();
+</script>
+<template>
+  <section
+    class="bg-zinc-800 w-64 h-[680px]"
+    id="movehistory">
+    <p class="text-center font-bold text-xl">Move History</p>
+    <ol class="list-decimal">
+      <li
+        v-for="(move, index) in gs.moveHistory"
+        :key="index">
+        {{ move[0] || "" }}
+        {{ move[1] || "" }}
+      </li>
+    </ol>
+  </section>
+</template>
+
+<style>
+#movehistory {
+  grid-area: moves;
+  @apply ml-10 p-10 pt-5 text-white;
+}
+</style>
