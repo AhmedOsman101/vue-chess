6f137bebdc7c9c5a40613fcc51c5f3e8f9c11066
AhmedOsman101 <trollingosman101@outlook.com>
2025-02-10 16:07:18 +0200

    Updated Files 2025-02-10@04:07PM

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 export const getChar = (index: number): string => String.fromCharCode(index);
 export const char2num = (char: string): number => char.charCodeAt(0);
 
-export const isNumber = (str: string): boolean => !Number.isNaN(+str);
+export const isNumber = (value: string | number) => !Number.isNaN(+value);
 
 export function* range(
   start = 0,
@@ -14,3 +14,19 @@ export function* range(
     i += step;
   }
 }
+
+export const getUnique = (arr: number[]): number | undefined => {
+  let zeroCount = 0;
+  const unique = arr.reduce((a, b) => {
+    if (b === 0) zeroCount++;
+    return a ^ b;
+  });
+  if (zeroCount > 1 && unique === 0) return undefined;
+
+  return unique;
+};
+
+// Function to generate random integers within a range
+export const getRandomInt = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
